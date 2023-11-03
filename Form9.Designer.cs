1d5e48bdca18a6aa2ee853e6f9060dd94a62a316
Rumtinus <rumtinazad@gmail.com>
2023-11-03 15:19:04 +0330

    change button text in form9

--- Form9.Designer.cs
+++ Form9.Designer.cs
@@ -76,7 +76,7 @@
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 3;
-            button1.Text = "button1";
+            button1.Text = "حذف";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
